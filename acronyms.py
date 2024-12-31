2d680ab014ea2c643af0e2467b34a2b016fa1e98
manjuiitm <24ds2000126@ds.study.iitm.ac.in>
2024-12-31 12:01:30 +0300

    Update acronyms.py

--- acronyms.py
+++ acronyms.py
@@ -1,3 +1,5 @@
+###To create acronyms using Python, you need to write a python program that generates a short form of a word from a given sentence. You can do this by splitting and indexing to get the first word and then combine it. Let’s see how to create an acronym using Python:
+
 user_input = str(input("Enter a Phrase: "))
 text = user_input.split()
 a = " "
