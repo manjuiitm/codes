8b66341f61df626369c338e5b8ff94d9a7b300e6
manjuiitm <24ds2000126@ds.study.iitm.ac.in>
2024-12-31 12:22:52 +0300

    Update coloredtext.py

--- coloredtext.py
+++ coloredtext.py
@@ -2,6 +2,6 @@ import colorama
 from colorama import Fore, Back, Style
 colorama.init(autoreset=True)
 
-print(Fore.BLUE+Back.YELLOW+"Hi My name is Manju Thomas "+ Fore.YELLOW+ Back.BLUE+"I am your Machine Learning Instructor")
+print(Fore.BLUE+Back.YELLOW+"Hi My name is Manju Thomas "+ Fore.YELLOW+ Back.BLUE+"I am a Data Scientist")
 print(Back.CYAN+"Hi My name is Manju Thomas")
 print(Fore.RED + Back.GREEN+ "Hi My name is Manju Thomas")
